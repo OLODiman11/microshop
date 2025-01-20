661dbd500a55910e3924ba91954835e53b19faaa
OLODiman11 <OLODiman11@mail.ru>
2025-01-20 16:32:37 +0300

    feat: kafka

---
+++ order-service/src/main/java/by/olodiman11/microshop/orderservice/event/OrderPlacedEvent.java
@@ -0,0 +1,12 @@
+package by.olodiman11.microshop.orderservice.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderPlacedEvent {
+    private String orderNumber;
+}
